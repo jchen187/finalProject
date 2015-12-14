05240d927bcbdc7354ade2187ec28599c42bd425
Johnny Chen <j_chen187@yahoo.com>
2015-12-13 23:02:32 -0500

    I now have a window that can reveal more data. Just need to incorporate it with the marker

--- menu.js
+++ menu.js
@@ -1,135 +1,29 @@
-(function(window) {
+(function(){
 
-  'use strict';
+  $(document).ready(function(){
+    //this is just a test
+      $("#over_map").click(function(){
+        console.log("hi");
 
-  /**
-   * Extend Object helper function.
-   */
-  function extend(a, b) {
-    for(var key in b) {
-      if(b.hasOwnProperty(key)) {
-        a[key] = b[key];
-      }
-    }
-    return a;
-  }
+        $("section").toggleClass("active");
+        // $(".content").toggleClass("showContent");
+      });
 
-  /**
-   * Each helper function.
-   */
-  function each(collection, callback) {
-    for (var i = 0; i < collection.length; i++) {
-      var item = collection[i];
-      callback(item);
-    }
-  }
+      // $(':not(.content)').click(function() {
+      //   // $().hide();
+      //   console.log("bye");
+      //   $("section").toggleClass("active");
+      // });
 
-  /**
-   * Menu Constructor.
-   */
-  function Menu(options) {
-    this.options = extend({}, this.options);
-    extend(this.options, options);
-    this._init();
-  }
+      //if press on x button
+      $(".close").click(function(){
+        console.log("bye");
 
-  /**
-   * Menu Options.
-   */
-  Menu.prototype.options = {
-    wrapper: '#o-wrapper',          // The content wrapper
-    type: 'slide-left',             // The menu type
-    menuOpenerClass: '.c-button',   // The menu opener class names (i.e. the buttons)
-    maskId: '#c-mask'               // The ID of the mask
-  };
+        $("section").toggleClass("active");
+        // $(".content").toggleClass("showContent");
 
-  /**
-   * Initialise Menu.
-   */
-  Menu.prototype._init = function() {
-    this.body = document.body;
-    this.wrapper = document.querySelector(this.options.wrapper);
-    this.mask = document.querySelector(this.options.maskId);
-    this.menu = document.querySelector('#c-menu--' + this.options.type);
-    this.closeBtn = this.menu.querySelector('.c-menu__close');
-    this.menuOpeners = document.querySelectorAll(this.options.menuOpenerClass);
-    this._initEvents();
-  };
+      });
 
-  /**
-   * Initialise Menu Events.
-   */
-  Menu.prototype._initEvents = function() {
-    // Event for clicks on the close button inside the menu.
-    this.closeBtn.addEventListener('click', function(e) {
-      e.preventDefault();
-      this.close();
-    }.bind(this));
-
-    // Event for clicks on the mask.
-    this.mask.addEventListener('click', function(e) {
-      e.preventDefault();
-      this.close();
-    }.bind(this));
-  };
-
-  /**
-   * Open Menu.
-   */
-  Menu.prototype.open = function() {
-    this.body.classList.add('has-active-menu');
-    this.wrapper.classList.add('has-' + this.options.type);
-    this.menu.classList.add('is-active');
-    this.mask.classList.add('is-active');
-    this.disableMenuOpeners();
-  };
-
-  /**
-   * Close Menu.
-   */
-  Menu.prototype.close = function() {
-    this.body.classList.remove('has-active-menu');
-    this.wrapper.classList.remove('has-' + this.options.type);
-    this.menu.classList.remove('is-active');
-    this.mask.classList.remove('is-active');
-    this.enableMenuOpeners();
-  };
-
-  /**
-   * Disable Menu Openers.
-   */
-  Menu.prototype.disableMenuOpeners = function() {
-    each(this.menuOpeners, function(item) {
-      item.disabled = true;
     });
-  };
 
-  /**
-   * Enable Menu Openers.
-   */
-  Menu.prototype.enableMenuOpeners = function() {
-    each(this.menuOpeners, function(item) {
-      item.disabled = false;
-    });
-  };
-
-  /**
-   * Add to global namespace.
-   */
-  window.Menu = Menu;
-
-})(window);
-
-// var slideRight = new Menu({
-//   wrapper: '#o-wrapper',
-//   type: 'slide-right',
-//   menuOpenerClass: '.c-button',
-//   maskId: '#c-mask'
-// });
-//
-// var slideRightBtn = document.querySelector('#c-button--slide-right');
-//
-// slideRightBtn.addEventListener('click', function(e) {
-//   e.preventDefault;
-//   slideRight.open();
-// });
+})();
